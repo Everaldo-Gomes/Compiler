260b081ed4c161b96d86aa2121418281aaae25c8
Everaldo-Gomes <everaldo332@gmail.com>
2021-09-01 22:29:11 -0300

    gera transição por S

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//string getString(char x);
-
 #define QNT_SIMBOLO 30
-#define LINHA 3
-#define COLUNA 13
+#define LINHA 8
+#define COLUNA 14
 
 string simbolos[QNT_SIMBOLO];
 string matriz_automato[LINHA][COLUNA];	
 void gerar_simbolo_token();
 void gerar_simbolo_glc();
 void gerar_matriz_automato_estados();
-int retorna_coluna_simbolo(char *s);
+int retorna_coluna_simbolo(string);
 
 short n = 0;
 
@@ -34,7 +32,7 @@ int main () {
 	/* mostra a matriz automato */
 	for(int i = 0; i < LINHA; i++) {
         for(int j = 0; j < COLUNA; j++) {
-			cout << matriz_automato[i][j];
+			cout << matriz_automato[i][j] << "\t";
         }
 		cout << "\n";
 	}
@@ -81,55 +79,70 @@ void gerar_simbolo_token() {
 
 void gerar_matriz_automato_estados() {
 
-	/* com problemas precisa consertar */
-	//s.push_back(c); std::cout << s << std::endl;
-	/* preenche a 1ª linha da matriz */
+	// gera 1ª linha do autômoto
 	for(int i = 1; i < COLUNA; i++) {
-		//printf("-%s-", (char[2]){simbolos[i-1], 0});
-		//char *p1 = simbolos;  
-		//printf("%c", p1[i-1]);
-		//matriz_automato[0][i] = p1[i-1];
+		matriz_automato[0][i] = simbolos[i-1];
 	}
+	
 	matriz_automato[0][0] = " ";
 	matriz_automato[1][0] = "S";
-
-	/* passando valores manualmente mas precisa-se passa no loop anterior */
-	matriz_automato[0][1] = "s";
-	matriz_automato[0][2] = "f";
-	matriz_automato[0][3] = "i";
-	matriz_automato[0][4] = "n";
-	matriz_automato[0][5] = "q";
-	matriz_automato[0][6] = "m";
-	matriz_automato[0][7] = "a";
-	matriz_automato[0][8] = "+";
-	matriz_automato[0][9] = "-";
-	matriz_automato[0][10] = "*";
-	matriz_automato[0][11] = "/";
-	matriz_automato[0][12] = "=";
-
-
+	
+	
 	/* adiciona os estados ao autômato */
 	FILE* arquivo = fopen("./arquivo.txt","rt");
 
-	char linha[5];
-	char tokens[5];
+	char linha[5];  //cada linha do arquivo
+	char tokens[5]; //faz um loop na string de cada linha (letra por letra)
+	char estado = 'A'; 
 
 	while(fgets(linha, 5, arquivo)) {
 
-		sscanf(linha, "%s", tokens);
-
+		sscanf(linha, "%s", tokens); //copia conteúdo da linha para o "tokens"
+	    
 		for (int i = 0; i < strlen(tokens); i++) {
+
+			//convert char em string
+			string s(1, tokens[i]); 
+			string novo_estado(1, estado);
 			
 			/* se for o 1º caracter do token, deve-se gerar uma transição pelo estado S */
 			if (i == 0) {
+				short coluna = retorna_coluna_simbolo(s);
 
-				//short p = retorna_coluna_simbolo((char[2]){tokens[i], 0});
-				//printf("%c ", (char[2]){tokens[i], 0});
-				//printf("%d <----\n", p);
-				//matriz_automato[1][p];
+				// evitar os tracinhos ou vígulas em lugares indevidos
+				if (matriz_automato[1][coluna] == "-") {
+					matriz_automato[1][coluna] = "";	
+				}
+
+				if (matriz_automato[1][coluna].size() == 0) {
+					matriz_automato[1][coluna].append(novo_estado);
+				}
+				else {
+					matriz_automato[1][coluna].append("," + novo_estado);
+				}
+				
+				
+				//string aux = 
+				
+				//coloca na transição de S uma transição pelo token encontrado
+				//matriz_automato[1][coluna] = estado;
+				estado++;
 			}
 		}
 	}
-
 	fclose(arquivo);
+}
+
+int retorna_coluna_simbolo(string s) {
+
+	short posicao = -1;
+	
+	for (int i = 1; i < COLUNA; i++) {
+		if (matriz_automato[0][i] == s) {
+			posicao = i;
+			break;
+		}
+	}
+
+	return posicao;
 }
