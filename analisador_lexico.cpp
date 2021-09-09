63388d3291198d6ef2d1408e00b46923590899d8
Everaldo-Gomes <everaldo332@gmail.com>
2021-09-08 22:22:38 -0300

    inicio analise sintatica

--- analisador_lexico.cpp
+++ analisador_lexico.cpp
@@ -30,17 +30,47 @@ void Analizador_lexico::analise_lexica() {
 
 	char linha[20];  //cada linha do arquivo
 	char tokens[20]; //faz um loop na string de cada linha (letra por letra)
-	
+	short contador_linha = 1;
+	string estado_inicial = "S";
+	string estado_corrente = "S";
+
 	while(fgets(linha, 20, arquivo)) {
 		
 		sscanf(linha, "%s", tokens); //copia conteúdo da linha para o "tokens"
 		strcpy(tokens,linha); //ajuste
 		
 		short tamanho_token = strlen(tokens);
+		string caracter_atual;
 		
 		for (int i = 0; i < tamanho_token; i++) {
+
+			caracter_atual = tokens[i];
+
+			// procurar na matriz a posição do caracter e estado
+			short posicao_caracter_coluna = retorna_coluna_simbolo(caracter_atual);
+			short posicao_estado_linha = retorna_linha_estado(estado_corrente);
+
+			if (caracter_atual != " " && caracter_atual != "\t" && caracter_atual != "\n") {
+				estado_corrente = automato[posicao_estado_linha][posicao_caracter_coluna];
+				cout << caracter_atual << " " << estado_corrente << endl;
+			}
 			
-			// lexico aqui
+			// verifica aceitação do token (se é separador ou não)
+
+			if (caracter_atual == " ") {
+
+				// se for aceiteação
+				// adiciona na fita de saída
+
+				//se for erro
+				//adiciona o erro na fita
+
+				// alterar o estado corrente pra S
+
+				//criar a fita de saída aceite e erro
+				
+				cout << verifica_estado_aceitacao(estado_corrente) << " " << estado_corrente << endl;
+			}
 		}
 	}
 
@@ -48,7 +78,53 @@ void Analizador_lexico::analise_lexica() {
 }
 
 
+short Analizador_lexico::retorna_coluna_simbolo(string s) {
+
+	short posicao = -1;
+	
+	for (int i = 1; i < COLUNA; i++) {
+		if (automato[0][i] == s) {
+			posicao = i;
+			break;
+		}
+	}
+
+	return posicao;
+}
 
 
+short Analizador_lexico::retorna_linha_estado(string s) {
 
+	short posicao = -1;
+	
+	for (int i = 1; i < LINHA; i++) {
 
+		string aux;
+		aux.append(s + "*");
+		
+		if (automato[i][0] == s || automato[i][0] == aux) {
+			posicao = i;
+			break;
+		}
+	}
+
+	return posicao;
+}
+
+short Analizador_lexico::verifica_estado_aceitacao(string s) {
+
+	short eh_aceitacao = 0;
+	
+	for (int i = 1; i < LINHA; i++) {
+
+		string aux;
+		aux.append(s + "*");
+		
+		if (automato[i][0] == aux) {
+			eh_aceitacao = 1;
+			break;
+		}
+	}
+
+	return eh_aceitacao;
+}
--- analisador_lexico.h
+++ analisador_lexico.h
@@ -16,6 +16,9 @@ private:
 	
 	string automato[LINHA][COLUNA];
 	string identificadores[QNT_SIMBOLO];
+	short retorna_coluna_simbolo(string s);
+	short retorna_linha_estado(string s);
+	short verifica_estado_aceitacao(string s);
 	
 	void analise_lexica(); // executa o algorítimo de análise léxica
 };
